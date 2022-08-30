c85241429b35dffc25362ec6a580050ff84eaab9
Reese Sorrell <joreesesorrell@gmail.com>
2022-08-30 16:46:15 -0500

    Show button presses in display window
    Add onclick which updates the display with any numbers or operators
    clicked by the user.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+const display = document.querySelector('.display');
+displayText = '';
 
 function add(a, b) {
     return a + b;
@@ -19,3 +20,12 @@ function divide(a, b) {
 function operate(func, a, b) {
     return func(a, b);
 }
+
+function updateDisplay(a) {
+    displayText += a.textContent;
+    console.log(a.textContent);
+}
+
+var updateTimer = setInterval(function() {
+    display.textContent = displayText;
+}, 100)
