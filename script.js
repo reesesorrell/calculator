0b469529ab66247b0dfa92a1eadefff20d582edb
Reese Sorrell <joreesesorrell@gmail.com>
2022-08-30 23:25:26 -0500

    Add clear function and restrict decimal usage
    Add a function to the clear button that resets the display screen
    back to an empty string. Restrict decimal usage to once per each number.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const display = document.querySelector('.display');
 displayText = '';
+decimalPresent = false;
+operator = false;
+
+functions = ['+', '-', 'x', '/']
 
 function add(a, b) {
     return a + b;
@@ -17,13 +21,28 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(func, a, b) {
-    return func(a, b);
+function operate(a, func, b) {
+    displayText = func(a, b);
 }
 
 function updateDisplay(a) {
-    displayText += a.textContent;
-    console.log(a.textContent);
+    if (functions.includes(a.textContent)) {
+        decimalPresent = false;
+        displayText += a.textContent;
+    }
+    else if (a.textContent == '.') {
+        if (!decimalPresent) {
+            displayText += a.textContent;
+            decimalPresent = true;
+        }
+    }
+    else {
+        displayText += a.textContent;
+    }
+}
+
+function clearDisplay() {
+    displayText = '';
 }
 
 var updateTimer = setInterval(function() {
